a04613aecf971968d54d19d5e40ad6654ccacf55
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-09-07 16:41:57 -0300

    feat: adicionando função para ler o arquivo

--- FILES/main.c
+++ FILES/main.c
@@ -11,3 +11,15 @@ FILE *openFIle(char nome[25], char mode[5]) {
 void closeFile(FILE *file) {
     fclose(file);
 }
+
+void readFile(char nome[25]) {
+    FILE *file = openFIle(nome, "r");
+
+    char frase[50];
+    while (fgets(frase, 10, file) != NULL)
+    {
+        printf("%s", frase);
+    }
+
+    closeFile(file);
+}
