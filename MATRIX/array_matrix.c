eb79bb4c077f46b4d6012277818fc37ce3eda897
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-09-08 19:47:47 -0300

    matrix: procedimento multiplyMatrix

--- MATRIX/array_matrix.c
+++ MATRIX/array_matrix.c
@@ -87,12 +87,28 @@ void subtractMatrix(int vet1[TAMANHO], int vet2[TAMANHO]) {
 /*
  - Multiplicação de matrizes
 
-                            col0                                                 col1                                                 col2
-row0 (r0,c0)*(r0,c0) + (r0,c1)*(r1,c0) + (r0,c2)*(r2,c0) | (r0,c0)*(r0,c1) + (r0,c1)*(r1,c1) + (r0,c2)*(r2,c1) | (r0,c0)*(r0,c2) + (r0,c1)*(r1,c2) + (r0,c2)*(r2,c2)
-row1 (r1,c0)*(r0,c0) + (r1,c1)*(r1,c0) + (r1,c2)*(r2,c0) | (r1,c0)*(r0,c1) + (r1,c1)*(r1,c1) + (r1,c2)*(r2,c1) | (r1,c0)*(r0,c2) + (r1,c1)*(r1,c2) + (r1,c2)*(r2,c2)
-row2 (r2,c0)*(r0,c0) + (r2,c1)*(r1,c0) + (r2,c2)*(r2,c0) | (r2,c0)*(r0,c1) + (r2,c1)*(r1,c1) + (r2,c2)*(r2,c1) | (r2,c0)*(r0,c2) + (r2,c1)*(r1,c2) + (r2,c2)*(r2,c2)
+                             col0                                                  col1                                                  col2
+row0  (r0,c0)*(r0,c0) + (r0,c1)*(r1,c0) + (r0,c2)*(r2,c0) | (r0,c0)*(r0,c1) + (r0,c1)*(r1,c1) + (r0,c2)*(r2,c1) | (r0,c0)*(r0,c2) + (r0,c1)*(r1,c2) + (r0,c2)*(r2,c2)
+row1  (r1,c0)*(r0,c0) + (r1,c1)*(r1,c0) + (r1,c2)*(r2,c0) | (r1,c0)*(r0,c1) + (r1,c1)*(r1,c1) + (r1,c2)*(r2,c1) | (r1,c0)*(r0,c2) + (r1,c1)*(r1,c2) + (r1,c2)*(r2,c2)
+row2  (r2,c0)*(r0,c0) + (r2,c1)*(r1,c0) + (r2,c2)*(r2,c0) | (r2,c0)*(r0,c1) + (r2,c1)*(r1,c1) + (r2,c2)*(r2,c1) | (r2,c0)*(r0,c2) + (r2,c1)*(r1,c2) + (r2,c2)*(r2,c2)
 
 */
+
+void multiplyMatrix(int vet1[TAMANHO], int vet2[TAMANHO]) {
+    for (int lop = 0; lop < 3; lop ++) {  //lop para pular de linha na impressão
+
+        for (int col = 0; col < 3; col ++) {  // a coluna permanece a mesme durante a interação, assim ele mude apenas 3 veses
+            int mult = 0;
+            for (int row = 0; row < 3; row ++) {  // a linha irá mudar, a cada mudansa na linha multiplico
+                mult += vet2[fetchPosition(row +1, col +1)] * vet1[fetchPosition(col +1, row +1)];
+            }
+            printf("%2d ", mult);
+        }
+        printf("\n");
+    }
+}
+
+
 
 void main() {
     srand(time(NULL));
